3ca3b682b795d0a017bdc97ba47122c8898c9ebb
john <zhaoxuan1727@gmail.com>
2013-09-23 22:46:05 +0800

    use new import

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-import sys;
-import os;
+import lib.mailer
 
-ROOT_PATH = os.getcwd()
-path = ROOT_PATH + "/lib"
-if not path in sys.path:
-  sys.path.append(path)
-if not 'mailer' in sys.modules:
-  import mailer
-
-mailer.mail("Hi john\n This is raspberry pi \n I am fine.")
-
-# import BeautifulSoup
-# import urllib2
-# import types
-
-# url = "http://www.baidu.com"
-# req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
-# webpage = urllib2.urlopen(req)
-
-# soup = BeautifulSoup.BeautifulSoup(webpage.read())
-
-# a_tags = soup.findAll("a")
-
-# print type(a_tags)
+lib.mailer.mail("Hi john\n This is raspberry pi \n I am fine.")
