6ddde5ffa07daea335e63365c9e1f04d820e1640
john <zhaoxuan1727@gmail.com>
2013-09-25 00:06:03 +0800

    initialize weather forecast

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import lib.mailer
+import lib.weather_forecast
 import time
 
 ts = time.strftime('%a, %d %b %Y %H:%M:%S %z')
-lib.mailer.mail("Hi john\n This is raspberry pi \n I am fine.\n" + ts)
+# lib.mailer.mail("This is raspberry pi.\n" + ts)
+
+wf = lib.weather_forecast.WeatherForecast()
+print wf.get_day_weather()
