0a69bd19aa1cea961472ba9b692b147401b4f5f6
zhaoxuan <zhaoxuan1727@gmail.com>
2013-09-24 17:29:44 +0800

    html parse

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-import lib.mailer
-import time
+#!/usr/bin/env python2.7
 
-ts = time.strftime('%a, %d %b %Y %H:%M:%S %z')
-lib.mailer.mail("Hi john\n This is raspberry pi \n I am fine.\n" + ts)
+import time
+import lib.mailer
+from lib.html import Html
+
+def main():
+    ts = time.strftime('%a, %d %b %Y %H:%M:%S %z')
+    # lib.mailer.mail("Hi john\n This is raspberry pi \n I am fine.\n" + ts)
+    html = Html("http://finance.baidu.com/")
+
+    content = html.find('a')
+    for item in content:
+        print item
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
