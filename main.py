7831a014bbbd5fd4fe97fe490833d3200842df44
zhaoxuan <zhaoxuan1727@gmail.com>
2013-09-25 09:25:03 +0800

    short information

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import lib.mailer
 import time
 
 ts = time.strftime('%a, %d %b %Y %H:%M:%S %z')
-lib.mailer.mail("Hi john\n This is raspberry pi \n I am fine.\n" + ts)
+lib.mailer.mail("This is raspberry pi.\n" + ts)
