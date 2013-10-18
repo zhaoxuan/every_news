8c657791954510833e1229de527db44faa7e6b60
zhaoxuan <zhaoxuan1727@gmail.com>
2013-10-18 15:06:37 +0800

    chinese to english

--- lib/weather_forecast.py
+++ lib/weather_forecast.py
@@ -13,7 +13,7 @@ class WeatherForecast(object):
     def __init__(self):
         # url = 'http://m.weather.com.cn/data/101010100.html'
         url = 'http://www.weather.com.cn/data/cityinfo/101010100.html'
-        headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+        headers = {'user-agent':'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'}
 
         self.req = urllib2.Request(url, headers = headers)
 
@@ -33,7 +33,7 @@ class WeatherForecast(object):
                 weather_html = urllib2.urlopen(self.req).read()
                 weather_json = json.loads(weather_html)
                 weather_info = weather_json['weatherinfo']
-                result = weather_info['city'] + u' 温度 ' + weather_info['temp2'] + '~' + weather_info['temp1']
+                result = weather_info['city'] + ' Temperature ' + weather_info['temp2'] + '~' + weather_info['temp1']
                 return result
             except Exception:
                 if try_times < 3:
