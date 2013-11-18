29ca689bce291b28c3c6daeea8d9d248b4b88b30
zhaoxuan <zhaoxuan1727@gmail.com>
2013-11-18 16:28:04 +0800

    use stock script

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import lib.weather_forecast
 from lib.stock import Stock
 from lib.file import File
 import os
+import pdb
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -16,28 +17,47 @@ ROOT = sys.path[0]
 
 def main():
     param = sys.argv[1]
-
-    try:
-        if param == 'weather':
-            weather()
-        else:
-            stock()
-            pass
-    except Exception, e:
-        # lib.mailer.mail("Every news has an error {0}".format(str(e)))
-        raise e
+    stock()
+    # try:
+    #     if param == 'weather':
+    #         weather()
+    #     else:
+    #         stock()
+    #         pass
+    # except Exception, e:
+    #     # lib.mailer.mail("Every news has an error {0}".format(str(e)))
+    #     raise e
 
 def stock():
     # info_table = ['name', 'price', 'advance_decline', 'range', 'total', 'amount']
     # st = Stock('sh', '600036')
     # stock_info = st.get_info()
 
-    f = File(ROOT + '/log/lig/stock.txt')
-    f.write('1234\n')
-    # f = open(ROOT + '/log/stock.txt', 'a')
-    # f.write("asdf\n")
+    # f = File(ROOT + '/log/stock.txt')
+    # f.write('\t'.join(stock_info) + '\n')
+
     # f.close
 
+    stocks = open(ROOT + '/log/StockCode', 'r')
+    f = File(ROOT + '/log/stock.txt')
+
+    while True:
+        line = stocks.readline()
+        if not line:
+            break
+        else:
+            exchange = line[0:2]
+            code = line[2:]
+            st = Stock(exchange, code)
+            stock_info = st.get_info()
+
+            if stock_info == None:
+                pass
+            else:
+                f.write('\t'.join(stock_info) + '\n')
+
+    f.close
+    stocks.close
     pass
 
 def weather():
