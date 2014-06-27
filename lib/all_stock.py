8def6637d22969d960904fec028c4992ae6e275b
zhaoxuan <zhaoxuan1727@gmail.com>
2014-06-27 20:59:19 +0800

    new way to get all stock

--- lib/all_stock.py
+++ lib/all_stock.py
@@ -1,6 +1,8 @@
 from HTMLParser import HTMLParser
 import urllib2
 import sys
+import re
+import string
 
 class MyHTMLParser(HTMLParser):
     def __init__(self):
@@ -9,24 +11,35 @@ class MyHTMLParser(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
         #print "Encountered the beginning of a %s tag" % tag
+        pattern = re.compile(r'http://bbs.10jqka.com.cn/')
+
         if tag == "a":
             if len(attrs) == 0: pass
             else:
-                for (variable, value)  in attrs:
-                    if variable == "name":
-                        self.links.append(value)
+                for (variable, value) in attrs:
+                    match = pattern.split(value)
+                    if len(match) == 2:
+                        tmp = match[1].split(',')
+                        if tmp[0] != 'fu':
+                            self.links.append(string.join(tmp[0:2], ''))
 
 if __name__ == "__main__":
-    a = ['sha0','sha1','sha2','sha3','sha4','sha5','sha6','sha7','sha8','sha9','sha10','sha11','sha12','sha13','sha14','sha15','sha16','sza0','sza2','sza3','sza4','sza5','sza6','sza7','sza8','sza9']
     root = sys.path[0]
     f = open(root + '/../log/StockCode', 'a')
-    for i in a:
-        url = 'http://stock.sina.com.cn/stock/quote/' + i + '.html'
-        html_code = urllib2.urlopen( url ).read()
-        hp = MyHTMLParser()
-        hp.feed(html_code)
-        hp.close()
-        for item in hp.links:
-            f.write(item + '\n')
-        pass
+    url = 'http://bbs.10jqka.com.cn/codelist.html'
+    html_code = urllib2.urlopen( url ).read()
+    hp = MyHTMLParser()
+    hp.feed(html_code)
+    hp.close()
+    for item in hp.links:
+        f.write(item + '\n')
     f.close
+
+
+
+
+
+
+
+
+
